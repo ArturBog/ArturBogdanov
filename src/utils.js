a882e203b4697ab1504c24b307ede23f8127755c
ArturBog <127194667+ArturBog@users.noreply.github.com>
2025-06-20 21:02:32 +0300

    Delete src directory

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Генерирует уникальный идентификатор
- * @returns {string} Уникальный идентификатор
- */
-export const generateId = () => {
-  // Генерируем случайную строку на основе текущего времени и случайного числа
-  return Date.now().toString(36) + Math.random().toString(36).substring(2);
-};
